4bdf2829b6d53c83da3688d782e986c26adf7c52
kyomei <rafa.jefer@gmail.com>
2019-01-08 15:49:17 -0200

    Page fale-conosco pre configurada

--- resources/views/fale-conosco.blade.php
+++ resources/views/fale-conosco.blade.php
@@ -2,12 +2,60 @@
 
 @section('title', 'Fale conosco')
 
-
-
 @section('conteudo')
 @component('components.breadcrumb') 
-    <li class="breadcrumb-item"><a href="{{url('/')}}">Home</a></li>
-    <li class="breadcrumb-item active">Fale conosco</li>
+<li class="breadcrumb-item"><a href="{{url('/')}}">Home</a></li>
+<li class="breadcrumb-item active">Fale conosco</li>
 @endcomponent
+<!-- START CONTENT FALE CONOSCO -->
+<section class="area-contact">
+    <div class="container">
+        <div class="row">
+            <!-- Entre em contato -->
+            <div class="col-md-8">
+                <h1><i class="fa fa-phone"></i> Nós ligamos para você!</h1>
+                <p>Informe seus dados, para que um de nossos especialistas entre em contato e realize uma avaliação gratuita. <strong>Nosso atendimento é Segunda à Sábado das 08:00 ás 17:00.</strong></p>
+                <h2>Campo de preenchimento</h2>
+                <form method="post" action="https://ieatprofissionalizante.com.br/fale-conosco/enviado">
+                    <div class="form-group">
+                        <label for="nome">Nome completo:</label>
+                        <input type="text" class="form-control" name="nome" id="nome"  aria-required="true" required="required" placeholder="Exemplo: João da Silva" maxlength="60">
+                    </div>
+                    <div class="form-group">
+                        <label for="email">Email (opcional):</label>
+                        <input type="email" class="form-control" name="email" id="email" aria-required="true" required="required" placeholder="Exemplo: exemplo@email.com" maxlength="80">
+                    </div>
+                    <div class="form-row">
+                        <div class="form-group col-md-6">
+                            <label for="assunto">Assunto:</label>
+                            <input type="text" class="form-control" name="assunto" id="assunto" aria-required="true" required="required"  placeholder="Assunto" />
+                        </div>
+                        <div class="form-group col-md-6">
+                            <label for="celular">Celular  WhatsApp:</label>
+                            <input type="tel" class="form-control phone" name="celular" id="celular" aria-required="true" required="required" placeholder="Número" maxlength="16">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="mensagem">Mensagem: </label>
+                        <textarea class="form-control" rows="5" id="mensagem" name="mensagem" maxlength="255" required="required" placeholder="Digite sua mensagem" ></textarea>
+                    </div>
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-primary btn-lg">Enviar</button>
+                    </div>
+                </form>
+            </div>
+            <div class="col-md-4">
+                <aside class="atendimento">			
+                    <h1><i class="fa fa-headphones"></i> Central de atendimento</h1>
+                    <p>Segunda à Sábado das 08:00 ás 17:00 </p>
+                    <p><i class="fa fa-phone"></i> (11) 2551-9764 <br class="visible-xs"><i class="fab fa-whatsapp"></i> (11) 9 4030-1118 <br><i class="fas fa-envelope"></i>  contato@ieatprofissionalizante.com.br</p>
 
+                    <h1><i class="fas fa-map-marker"></i> Localidade</h1>
+                    <p>Rua Doutor Meira pena, 29 - Vila Lourdes - Guaianases - SP <br /></p>
+                </aside>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- END./ CONTENT FALE CONOSCO -->
 @stop
--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -2,7 +2,7 @@
 <!-- Navigation -->
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark ">
     <div class="container">
-        <a class="navbar-brand" href="#">Start Bootstrap</a>
+        <a class="navbar-brand" href="#">&nbsp;</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -15,7 +15,7 @@
                 <li class="nav-item {{ Request::segment(1) === 'quem-somos' ? 'active' : null }}">
                     <a class="nav-link" href="{{url('quem-somos')}}">Quem somos</a>
                 </li>
-                <li class="nav-item">
+                <li class="nav-item {{ Request::segment(1) === 'fale-conosco' ? 'active' : null }}">
                     <a class="nav-link" href="{{url('fale-conosco')}}">Fale conosco</a>
                 </li>
                 <li class="nav-item dropdown {{ Request::segment(1) === 'cursos' ? 'active' : null }}">
