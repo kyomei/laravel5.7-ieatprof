1410312eae010ae1fc8bdd472f1ed427640a2d70
kyomei <rafa.jefer@gmail.com>
2019-01-08 09:05:51 -0200

    Incluindo primeiras view e SASS no projeto

--- resources/views/cursos-livres.blade.php
+++ resources/views/cursos-livres.blade.php
@@ -4,14 +4,16 @@
 
 @section('conteudo')
 @component('components.breadcrumb') 
-
+    <li class="breadcrumb-item"><a href="{{url('/')}}">Home</a></li>
+    <li class="breadcrumb-item"><a href="{{url('cursos/')}}">Cursos</a></li>
+    <li class="breadcrumb-item active">Cursos livres</li>
 @endcomponent
 <div class="container">
     <h1>Listagem de cursos livres</h1>
     <!-- list cursos  --> 
-    @component('components.card') 
-
-    @endcomponent
+    <article>
+        <img src="https://ieatprofissionalizante.com.br/assets/img/cursos/agente-de-turismo.png"/>
+    </article>
     <table class="table table-striped table-bordered table-hover">            
         @foreach ($modulos as $modulo)
         <tr>
--- resources/views/fale-conosco.blade.php
+++ resources/views/fale-conosco.blade.php
@@ -3,7 +3,11 @@
 @section('title', 'Fale conosco')
 
 
+
 @section('conteudo')
-<p>Fale conosco</p>
+@component('components.breadcrumb') 
+    <li class="breadcrumb-item"><a href="{{url('/')}}">Home</a></li>
+    <li class="breadcrumb-item active">Fale conosco</li>
+@endcomponent
 
 @stop
